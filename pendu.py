bd7d542121bd6d0d7b47bb369c5c05fa624dd574
hp28190 <hp28190@hotmail.fr>
2018-10-17 17:25:49 +0200

    Ajout parenthèse de fermeture du print Modification de l'ouverture du fichier score pour eviter les erreurs car il ne contient pas de dictionnaire ou autre.

--- pendu.py
+++ pendu.py
@@ -1,32 +1,31 @@
 #!/usr/bin/python3.7
 # -*-coding:Utf8 -*
 
-############################################
-##Fichier du jeu du pendu fait par LOUIS Maxime
-##
-##Dans le cadre du cours sur python d'openclassroom
-#############################################
-#############Déclaration nécessaires
-import random
-import os
 
+#Fichier du jeu du pendu fait par LOUIS Maxime
+#
+#Dans le cadre du cours sur python d'openclassroom
+
+#Déclaration nécessaires
+import random
+import pickle
+import os
 
 
 print("Bienvenu dans le jeu du pendu. \n")
 print('Chargement des fichiers\n')
 
-#####Vérification de la présence des fichiers
-if not os.path.exists('scores'):	##créé le fichier seulement si il n'existe pas
+#Vérification de la présence des fichiers
+if not os.path.exists('scores'):	#créé le fichier seulement si il n'existe pas
 	fichier = open("scores",'w')
-	close(fichier)
-
-
-#####Chargement des scores
-with open('score', 'rb') as fichier_score:
-	depickler = pickle.Unpickler(fichier_score)
-	scores = depickler.load()
+	fichier.close()
+else:
+#Chargement des scores
+	with open('scores', 'rb') as fichier_score:
+		depickler = pickle.Unpickler(fichier_score)
+		scores = depickler.load()
 	
-#####Chargement du dictionnaire et du nombre de coups
+#Chargement du dictionnaire et du nombre de coups
 with open('donnees.py', 'rb') as fichier_donnees:
 	depickler = pickle.Unpickler(fichier_donnees)
 	chances = depickler.load()	
@@ -35,17 +34,17 @@ with open('donnees.py', 'rb') as fichier_donnees:
 
 print('Fin du chargement des fichiers\n')
 
-joueur = input('Saisir votre nom de joueur')
+joueur = input('Saisir votre nom de joueur\n')
 	
 if joueur not in scores.keys():
 	score[joueur] = 0
 else:
-	print('Rebonjour {0}, votre score actuel est de {1} points'.format(joueur , score[joueur])
+	print("Rebonjour {0}, votre score actuel est de {1} points\n".format(joueur, score[joueur]))
 
-#########################Choix du mot et début du jeu
-arret = 'n'
+#Choix du mot et début du jeu
+fin_partie = 'n'
 
-while arret != 'o' :
+while fin_partie != 'o' :
 	tentatives = 0
 	lettres =[]
 	#mot = mots[random.randrange(mots.len())]
@@ -71,9 +70,9 @@ while arret != 'o' :
 				affichage_mot_masque(mot, lettres)
 				print('il vous reste',chances-tentatives,'chances de le trouver')			
 
-	arret = input('Voulez vous arreter? (o/n)')
+	fin_partie = input('Voulez vous arreter? (o/n)')
 ############## Sauvegarde des scores, peut être faire une fonction
-with open ('score', 'wb') as fichier_score:
+with open ('scores', 'wb') as fichier_score:
 	pickle = pickle.pickler(fichier_score)
 	pickle.dump(scores)
 	
