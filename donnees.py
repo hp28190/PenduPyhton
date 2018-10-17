cafe84b25572036c03f9816b7096709312ae22ff
hp28190 <hp28190@hotmail.fr>
2018-10-17 14:47:43 +0200

    Ajout du nombre de chance et du dictionnaire de mots dnas le fichier donnees.py

--- donnees.py
+++ donnees.py
@@ -1,2 +1,24 @@
 #!/usr/bin/python3.7
 # -*-coding:Utf8 -*
+
+chances = 10
+
+mots =
+[ sapin,
+  boulles,
+  verre,
+  ordinateur,
+  peluche,
+  papillon,
+  jardin,
+  langue,
+  anatomie,
+  clavier,
+  manette,
+  jeux,
+  pomme,
+  poire,
+  enceinte,
+  robot,
+  couverts,
+  casserole ]
