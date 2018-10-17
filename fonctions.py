70ffa8b9ae878364bffc18104c4c53d9f67f997d
hp28190 <hp28190@hotmail.fr>
2018-10-17 16:37:26 +0200

    Ajout d'une fonction (affichage_mot_masque) affichant le mot cherché avec des étoiles ou des caractères dans le fichier fonctions.py

--- fonctions.py
+++ fonctions.py
@@ -7,3 +7,20 @@
 ##Fait partie du projet PenduPyhton 
 ##########################################################
 
+def affichage_mot_tentatives (mot, lettres_trouves):
+	"""Cette fonction vérifie et créé un mot avec les lettres toruvé et 
+	des étoiles pour celles qui ne le sont pas
+
+	prend en paramètre: - un str contenant le mot 
+						- une list contenant les lettres trouvées
+						
+	"""
+	for lettre in mot:
+		if lettre in lettres_trouves:
+			aff_mot += lettre
+		else:
+			aff_mot += '*'
+	
+	print('Le mot que l\'on cherche est: {0}',.format(aff_mot))
+		
+
